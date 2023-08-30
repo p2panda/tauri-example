d058bd70de07cc13c46f9e51370ad880482490df
adz <x12@adz.garden>
2023-08-30 16:13:18 +0200

    Embed node and creatae shirokuma session

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use aquadoggo::{Configuration, Node};
+use p2panda_rs::identity::KeyPair;
+use tauri::async_runtime;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,6 +12,14 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    async_runtime::spawn(async {
+        let config = Configuration::default();
+        let key_pair = KeyPair::new();
+        let node = Node::start(key_pair, config).await;
+        node.on_exit().await;
+        node.shutdown().await;
+    });
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
