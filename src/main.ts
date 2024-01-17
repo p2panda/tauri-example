d6fe98d32353309865cb54dda2760ee7c842bd52
Sam Andreae <contact@samandreae.com>
2024-01-17 14:13:07 +0000

    Remove React framework

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { KeyPair } from "shirokuma";
+
+export const main = () => {
+	const keyPair = new KeyPair();
+	const publicKey = keyPair.publicKey();
+	console.log(`You are: ${publicKey}`);
+};
