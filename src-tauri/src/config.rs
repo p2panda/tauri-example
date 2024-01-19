adf84a5e7a69edbaac89cf87c0a445dc720b922f
Sam Andreae <contact@samandreae.com>
2024-01-19 12:57:07 +0000

    Load config from file and env variables

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -1,56 +1,47 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 use std::fs;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 use anyhow::Result;
 use aquadoggo::{ConfigFile, Configuration};
+use figment::providers::{Env, Serialized};
+use figment::Figment;
 use tauri::AppHandle;
 
 use crate::consts::{AQUADOGGO_CONFIG, BLOBS_DIR, RESOURCES_DIR};
 
-/// Load and validate an `aquadoggo` node configuration from .toml file.
-fn load_config_file(config_path: &Path) -> Result<Configuration> {
-    let config_str = fs::read_to_string(config_path)?;
-    let node_config: ConfigFile = toml::from_str(&config_str)?;
-    node_config.try_into()
-}
-
-/// Load config file from app data directory if it exists, if not copy
-/// default config.toml into the passed path and load it.
-pub fn load_config(
-    app: &AppHandle,
-    app_data_path: Option<&PathBuf>,
-) -> Result<Configuration, anyhow::Error> {
+/// Get configuration from 1. .toml file, 2. environment variables (in that order, meaning that
+/// later configuration sources take precedence over the earlier ones).
+pub fn load_config(app: &AppHandle, app_dir_path: PathBuf) -> Result<Configuration> {
     let default_config_path = app
         .path_resolver()
         .resolve_resource(PathBuf::new().join(RESOURCES_DIR).join(AQUADOGGO_CONFIG))
         .expect("failed to resolve resource");
 
-    if let Some(path) = app_data_path {
-        // This is the path where we expect our config file to be.
-        let config_path = path.join(AQUADOGGO_CONFIG);
+    // Determine if a config file path was provided or if we should look for it in common locations
+    let config_file_path = app_dir_path.join(AQUADOGGO_CONFIG);
 
-        // Check if the expected config file exists. If not, this is the first time
-        // running the app and we want to copy the default into place.
-        if fs::read(&config_path).is_err() {
-            fs::copy(default_config_path, &config_path)?;
-        }
+    // Check if the expected config file exists. If not, this is the first time
+    // running the app and we want to copy the default into place.
+    if !config_file_path.exists() {
+        fs::copy(default_config_path, &config_file_path)?;
+    };
 
-        // Now we can load the config file.
-        let mut config = load_config_file(&config_path)?;
+    let config_str = fs::read_to_string(config_file_path)?;
+    let mut config: ConfigFile = toml::from_str(&config_str)?;
 
-        // Override database url based on app data directory path.
-        config.database_url = format!(
-            "sqlite:{}/db.sqlite3",
-            path.to_str().expect("invalid character in path")
-        );
+    // Override database url based on app data directory path.
+    config.database_url = format!(
+        "sqlite:{}/db.sqlite3",
+        app_dir_path.to_str().expect("invalid character in path")
+    );
 
-        // Override blobs path based on app data directory path.
-        config.blobs_base_path = path.join(BLOBS_DIR);
+    // Override blobs path based on app data directory path.
+    config.blobs_base_path = Some(app_dir_path.join(BLOBS_DIR));
 
-        Ok(config)
-    } else {
-        load_config_file(&default_config_path)
-    }
+    // Merge the config file with any environment variables.
+    let figment = Figment::from(Serialized::defaults(config));
+    let config: ConfigFile = figment.merge(Env::raw()).extract()?;
+    config.try_into()
 }
